552f8f29913c3e346df73d0faf66dc6714eb898d
kwould <kcasado@squarespace.com>
2023-02-22 16:50:13 -0800

    fixing lua port of sourcecode
    commit-id:e490e641

--- nvim/lua/kwould/kwould.lua
+++ nvim/lua/kwould/kwould.lua
@@ -21,21 +21,22 @@ end
 
 local M  = {}
 
-M.search_source_repo = function()
+M.search_source_repo = function(branch)
 	local prefix = "https://github.com"
 	local full_filepath = vim.fn.expand('%:p')
 	local fileArr = table.split(full_filepath, "/")
 	local repo = fileArr[4]
-	-- print("here")
-	-- print(unpack(fileArr, 5, table.getn(fileArr)))
 	local filename = table.concat(table.slice(fileArr, 5, #fileArr), "/")
-	local gitBranch = "master"
+	local gitBranch = branch 
+	if branch == "curr" then
+		-- get current branch
+		gitBranch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+	end
 	local configurl = os.getenv("WORK_GH_URL")
 	local url = string.format("%s/%s/%s/blob/%s/%s", prefix, configurl, repo, gitBranch, filename)
-	local finalUrl = url .. "#" .. "L" .. vim.fn.line(".")
-	-- print(finalUrl)
-	print('silent exec "!open \'' .. finalUrl .. '\'"')
-	vim.cmd('silent exec "!open \'' .. url .. '\'"')
+	local finalUrl = url .. "\\#" .. "L" .. vim.fn.line(".")
+	local cmdStr =string.format("silent exec \'!open %s \'", finalUrl)
+	vim.cmd(cmdStr)
 end
 
 return M
